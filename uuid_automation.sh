a50f05bb2a5d43097fc562dbf3318467df1a901b
Pavan Kumar Paluri <pvpk1994@gmail.com>
2020-04-11 15:47:50 -0500

    Now supports automation of CPU IDs for Single and Multi Core RRP schedulers

--- uuid_automation.sh
+++ uuid_automation.sh
@@ -10,22 +10,29 @@
 
 UUID_INFO=$(pwd)/pool_uuid/uuid_info.txt
 POOLID_INFO=$(pwd)/pool_uuid/pool_id.txt
+CPU_LIST=$(pwd)/pool_uuid/cpu_list.txt
 
 if [ $(whoami) != "root" ]; then
 	echo "only root access!!!"
 	exit 1
 else
+        # ******* Extraction for CPU-IDs for RRP-XEN 2.0 ************* #
+        xl cpupool-list -c > $CPU_LIST
+        # ****** Extractions for Pool-ID and UUIDs ********* #
 	xl list -v -c > $(pwd)/pool_uuid/list_info.txt
 	NUM_RECORDS=$(cat $(pwd)/pool_uuid/list_info.txt | awk 'END{print NR}')
 	echo "total number: $NUM_RECORDS"
 	#cat list_info.txt | awk '{ for(counter = 0; counter < 3; counter++) {if ($10=="aaf_pool") {print $7}}}' >$UUID_INFO
 	cat $(pwd)/pool_uuid/list_info.txt | awk '{if($10=="aaf_pool") {print $11}}' >${POOLID_INFO}
+        # ******** FOR PCPUs **************** #
+        cat $CPU_LIST | awk '{if($1=="aaf_pool") {print $2}}' > $(pwd)/pool_uuid/rrp_cpus_list.txt
+        # *********************************** #
         for i in $(seq 1 $NUM_RECORDS); do
 		 cat $(pwd)/pool_uuid/list_info.txt | awk '{if($10=="aaf_pool") {print $7}}' >$UUID_INFO
 	done
 	if [ $? -eq 0 ]; then
 		echo " UUIDs of domain(s) and cpupool ID bought into simulation file"
-		gcc $(pwd)/aaf_prefinal.c -lxenctrl -lm -luuid -o aaf -L /usr/local/lib 2>/dev/null
+		gcc $(pwd)/aaf_new_single_resource.c -lxenctrl -lm -luuid -o aaf -L /usr/local/lib 2>/dev/null
 		if [ $? -eq 0 ]; then
 			echo "aaf schedul entries are set successfully!!"
 			echo "----proceeding with execution---"
@@ -40,4 +47,3 @@ else
 		echo "UUID(s) and pool-id import failure"
 	fi
 fi
-
